10c800487d002fe66351b4e89bb821f2066500f5
Gabriel <gabrielbrasil.dev@gmail.com>
2021-01-26 02:07:02 -0300

    Primeira aula da Imersão React / Alura

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import db from '../db.json'
 import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
@@ -20,6 +21,8 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+
+        <QuizLogo />
 
         <Widget>
           <Widget.Header>
@@ -41,7 +44,7 @@ export default function Home() {
 
         <Footer />
 
-        <GitHubCorner />
+        <GitHubCorner projectUrl="https://github.com/gabriel-brasil/thesimpsonsquiz" />
 
       </QuizContainer>
     </QuizBackground>
