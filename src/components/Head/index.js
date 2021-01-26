9a7b87a5c287a0fe984a0db99e5177fdbf11e1ee
Gabriel <gabrielbrasil.dev@gmail.com>
2021-01-26 11:27:53 -0300

    Ajustar meta tags

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -14,12 +14,9 @@ function MetaTags() {
       <meta property="og:title" content="The Simpsons" />
       <meta property="og:description" content="Projeto criado durante a Imersão React v2 da Alura." />
       <meta property="og:image" content="https://github.com/gabriel-brasil/thesimpsonsquiz/blob/main/src/images/print-projeto.jpg" />
-      {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://thesimpsonsquiz.gabriel-brasil.vercel.app/" />
-      <meta property="twitter:title" content="The Simpsons" />
-      <meta property="twitter:description" content="Projeto criado durante a Imersão React v2 da Alura." />
-      <meta property="twitter:image" content="https://github.com/gabriel-brasil/thesimpsonsquiz/blob/main/src/images/print-projeto.jpg" />
+      <meta property="fb:app_id" content="010203040506070809" />
+
+
     </Head>
   )
 
