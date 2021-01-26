7de0b569155280389f2cfe37dd39e36798cdf834
Gabriel <gabrielbrasil.dev@gmail.com>
2021-01-26 01:42:00 -0300

    Primeira aula da Imersão React / Alura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>
+            <h1>The Simpsons</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem Ipsum</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem Ipsum</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+
+        <GitHubCorner />
+
+      </QuizContainer>
+    </QuizBackground>
+  )
+
 }
